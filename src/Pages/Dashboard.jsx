83b3675e3f6e7a08b55d04b6988bb251ec2ceee4
nando <ham280207@gmail.com>
2025-09-23 15:37:57 +0700

    feat: membuat loader

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,13 +1,29 @@
 import Footer from "../components/Fragments/Footer/MainFooter"
+import { useState, useEffect } from "react";
 import MainNavbar from "../components/Fragments/NavBar/MainNavbar"
 
 const Dashoard = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {  
+        const timer = setTimeout(() => setLoading(false), 2000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) {
+        return (
+          <div className="flex flex-col items-center justify-center h-screen bg-white">
+            <div className="w-16 h-16 border-4 border-gray-300 rounded-full border-t-blue-500 animate-spin"></div>
+          </div>
+        );
+    }
+
     return (
         <>
-        <MainNavbar></MainNavbar>
+        <MainNavbar />
         <div className="my-90">           
         </div>
-        <Footer></Footer>
+        <Footer />
         </>
     )
 }
