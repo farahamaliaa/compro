f29db12be65e35162d34e0b06ceccefd50b5bd4d
nando <ham280207@gmail.com>
2025-09-23 10:47:44 +0700

    feat: install route dan navbar

---
+++ src/components/Fragments/NavBar/MainNavbar.jsx
@@ -0,0 +1,24 @@
+const MainNavbar = () => {
+    return (
+        <nav className="bg-background border-b border-border">
+
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between h-16 items-center">
+
+            <div className="flex-shrink-0">
+              <h1 className="text-2xl font-bold text-primary">Humma<span className="text-blue-700">Tech</span></h1>
+            </div>
+
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+
+                
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+}
+export default MainNavbar;
