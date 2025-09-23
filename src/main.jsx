51af628599b582c77276ccc9f3eb6b8504523dc2
IrfanDaffa <irfndappa@gmail.com>
2025-09-23 13:36:57 +0700

    feat: slicing structure

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import MainNavbar from './components/Fragments/NavBar/MainNavbar'
 import Dashoard from './Pages/Dashboard'
+import Structure from './Pages/About/Structure.jsx'
 
 
 const router = createBrowserRouter([
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
     path: "",
     element: <Dashoard></Dashoard>
   },
+  {
+    path: "structure",
+    element: <Structure></Structure>
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
